f18000a7fae1180cccb6f112a2998864ac64cd46
vaibhav gupta <133956419+vaibhavGuptagithub@users.noreply.github.com>
2024-07-04 17:11:45 +0530

    Update linux.sh

--- installation_scripts/linux.sh
+++ installation_scripts/linux.sh
@@ -20,6 +20,9 @@ sudo apt install -y curl
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 source ~/.bashrc                                                                  
 
+nvm install 20
+nvm use 20
+
 nvm install node
 nvm use node
 
